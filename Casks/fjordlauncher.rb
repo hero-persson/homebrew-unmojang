0dd1a951fce1d9a10c2f60d55450b1ea1e6d81c2
Evan Goode <mail@evangoo.de>
2026-01-10 12:19:33 -0500

    fjordlauncher: 9.4.2 -> 10.0.1.1

--- Casks/fjordlauncher.rb
+++ Casks/fjordlauncher.rb
@@ -1,13 +1,19 @@
 cask "fjordlauncher" do
-  version "9.4.2"
-
   on_catalina :or_older do
+    version "9.4.2"
     sha256 "a501e6a7e9e59c08e35e4a3201f831bae8bcf677c8229237d49018e0577c35f0"
 
     url "https://github.com/unmojang/FjordLauncher/releases/download/#{version}/FjordLauncher-macOS-Legacy-#{version}.zip"
   end
-  on_big_sur :or_newer do
+  on_big_sur do
+    version "9.4.2"
     sha256 "57285ca2d9f14ed9341f3ec8606da88cbb7e12121fa432ff80de1a913ad04174"
+
+    url "https://github.com/unmojang/FjordLauncher/releases/download/#{version}/FjordLauncher-macOS-#{version}.zip"
+  end
+  on_monterey :or_newer do
+    version "10.0.1.1"
+    sha256 "98f75c9054467f5fe106888a725438ff24e545c53366eca2c3b9af67d01926ae"
 
     url "https://github.com/unmojang/FjordLauncher/releases/download/#{version}/FjordLauncher-macOS-#{version}.zip"
   end
